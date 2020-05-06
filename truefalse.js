3539f5294c97e7ef2f4b55b6e950cad1042c04bf
masudwl <masudwl@gmail.com>
2020-05-06 10:57:31 -0700

    true false defined

---
+++ truefalse.js
@@ -0,0 +1,21 @@
+// falsy
+//false;
+//0; 
+//undefined; 
+//""; 
+//Null; 
+//NaN; 
+
+// Truthy
+//'0'; ' '; []; {}
+
+const name = 0; 
+
+if (name){
+    console.log("Condition is True"); 
+}
+else{
+    console.log("Condition is False"); 
+}
+
+
